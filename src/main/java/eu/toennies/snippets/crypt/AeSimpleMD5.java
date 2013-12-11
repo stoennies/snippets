b0e93ff277ed905cda95aa059604b273acfde6a5
stoennies <toennies@l3s.uni-hannover.de>
2013-12-11 09:45:55 +0100

    removed  dead store to local variable

--- src/main/java/eu/toennies/snippets/crypt/AeSimpleMD5.java
+++ src/main/java/eu/toennies/snippets/crypt/AeSimpleMD5.java
@@ -47,9 +47,8 @@ public class AeSimpleMD5 {
 			UnsupportedEncodingException {
 		MessageDigest md;
 		md = MessageDigest.getInstance("MD5");
-		byte[] md5hash = new byte[32];
 		md.update(text.getBytes("UTF-8"), 0, text.length());
-		md5hash = md.digest();
+		byte[] md5hash = md.digest();
 		return convertToHex(md5hash);
 	}
 }
